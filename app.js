8ea490ec05529dd16d8cf81ecb24f061c08f162d
Jeetesh <jeeteshkumar45@gmail.com>
2023-11-03 20:03:32 +0530

    cors updated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import taskRouter from "./routes/task.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 export const app = express();
 config({
@@ -12,6 +13,13 @@ config({
 // using middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(errorMiddleware);
 
 // routers
